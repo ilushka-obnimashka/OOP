df0795f9787ef0a29b2574c006125ce344cb22fd
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-12-04 23:21:53 +0700

    add 10 hw

---
+++ evolution/hw 10.1/creatures.h
@@ -0,0 +1,121 @@
+#pragma once
+/*generic creature - общий родитель
+ocean creature - наследник умеющий плавать
+amphibious - наследник умеющий плавать и ходить
+terrestrial creature - не умеет плавать, но умеет ходить
+bird - умеет ходить и летать
+waterfowl - водоплавающая птица. Умеет ходить, летать и плавать.*/
+
+
+
+class generic {
+public:
+    generic() {
+        std:: cout << "Generic Constructor called" << std:: endl;
+    }
+    ~generic() {
+        std:: cout << "Generic Deconstructor called" << std:: endl;
+    }
+
+    //virtual void eat() = 0; // закоментировано для 4 задания (не будет работать когда необходимо создать vector<generic> т.к.  данном случае generic - абстрактный класс)
+
+    // когда иллюстрируется полиморфное поведение , то этот метод можно закоментировать и разкоментировть чисто виртуальный, тогда generic станет абстрактным классом
+    virtual void eat() {
+        std::cout << "\033[35m";
+        std::cout << "generic eating" << std:: endl;
+        std::cout << "\033[0m";
+    }
+
+};
+
+class ocean : virtual public generic{
+public:
+    ocean() {
+        std:: cout << "Ocean creatures Constructor called" << std:: endl;
+    }
+    ~ocean() {
+        std:: cout << "Ocean creatures Deconstructor called" << std:: endl;
+    }
+
+    void swim () {
+        std:: cout << "I can swim" << std:: endl;
+    }
+
+    void eat() override {
+        std::cout << "\033[33m";
+        std::cout << "ocean creature eating" << std:: endl;
+        std::cout << "\033[0m";
+    }
+};
+
+class terrestrial : virtual public generic{
+public:
+    terrestrial() {
+        std:: cout << "Terrestrial creatures Constructor called" << std:: endl;
+    }
+    ~terrestrial() {
+        std:: cout << "Terrestrial creatures Deconstructor called" << std:: endl;
+    }
+    void walk() {
+        std:: cout << "I can walk" << std:: endl;
+    }
+
+    void eat() override {
+        std::cout << "\033[33m";
+        std::cout << "terrestrial creature eating" << std:: endl;
+        std::cout << "\033[0m";
+    }
+
+};
+
+class amphibious : public ocean, public terrestrial{
+public:
+    amphibious() {
+        std:: cout << "Amphibious creatures Constructor called" << std:: endl;
+    }
+    ~amphibious() {
+        std:: cout << "Amphibious creatures Deconstructor called" << std:: endl;
+    }
+
+    void eat() override {
+        std::cout << "\033[33m";
+        std::cout << "amphibious creature eating" << std:: endl;
+        std::cout << "\033[0m";
+    }
+};
+
+
+class bird : virtual public terrestrial{
+public:
+    bird() {
+        std:: cout << "Bird creatures Constructor called" << std:: endl;
+    }
+    ~bird() {
+        std:: cout << "Bird creatures Deconstructor called" << std:: endl;
+    }
+    void fly () {
+        std:: cout << "I can fly" << std:: endl;
+    }
+
+    void eat() override {
+        std::cout << "\033[33m";
+        std::cout << "bird creature eating" << std:: endl;
+        std::cout << "\033[0m";
+    }
+};
+
+class waterfowl: public bird, public ocean{
+public:
+    waterfowl() {
+        std:: cout << "Waterfowl creatures Constructor called" << std:: endl;
+    }
+    ~waterfowl() {
+        std:: cout << "Waterfowl creatures Deconstructor called" << std:: endl;
+    }
+    void eat() override {
+        std::cout << "\033[33m";
+        std::cout << "waterflow creature eating" << std:: endl;
+        std::cout << "\033[0m";
+    }
+};
+
---
+++ evolution/hw 10.1/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include "creatures.h"
+
+
+int main() {
+    /* иллюстрируется не полиморфное поведение*/
+    std::vector <generic> creatures;
+    amphibious a;
+    creatures.push_back(a);
+
+    terrestrial t;
+    creatures.push_back(t);
+
+    bird b;
+    creatures.push_back(b);
+
+    ocean o;
+    creatures.push_back(o);
+
+    waterfowl w;
+    creatures.push_back(w);
+
+    system("clear");// очистить сообщения конструкторов
+
+    for (size_t i =0 ; i<creatures.size(); i++) {
+        creatures[i].eat();
+        /* будет вызываться generic:: eat();
+    }
+
+    /* иллюстрируется полиморфное поведение*/
+    /*
+    std::vector <std::shared_ptr<generic>> creatures;
+
+    auto ptr_a = std::make_shared<amphibious>();
+    creatures.push_back(ptr_a);
+
+    auto ptr_t = std::make_shared<terrestrial>();
+    creatures.push_back(ptr_t);
+
+    auto ptr_b = std::make_shared<bird>();
+    creatures.push_back(ptr_b);
+
+    auto ptr_o = std::make_shared<ocean>();
+    creatures.push_back(ptr_o);
+
+    auto ptr_w = std::make_shared<waterfowl>();
+    creatures.push_back(ptr_w);
+
+    system("clear");// очистить сообщения конструкторов
+
+
+    for (size_t i =0 ; i<creatures.size(); i++) {
+        creatures[i]->eat();
+        /* будет вызываться eat() оответсвтующий конкретному экземпляру;
+    }*/
+
+}
