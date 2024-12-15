3ffed12b731e964dcfbfb783d4056795a0a35056
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-12-15 17:40:05 +0700

    fix ArgumentParser ArgumentParser()

---
+++ 3Lab_C++/ConfigParser.h
@@ -0,0 +1,11 @@
+#ifndef CONFIGPARSER_H
+#define CONFIGPARSER_H
+
+/**
+* @brief class CongigParser provides a convenient interface for parsing config file using std::regex.
+*/
+class ConfigParser {
+public:
+    ConfigParser(std::string congig_file)
+};
+#endif //CONFIGPARSER_H
