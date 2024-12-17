03ffad2ee6d45e2c03de3762e56d39fb70fad8b0
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-12-18 03:36:09 +0700

    Reformat code

--- construction/hw 12.1/developer.h
+++ construction/hw 12.1/developer.h
@@ -1,52 +1,64 @@
-class Developer {
+class Developer
+{
 public:
-    virtual House *BuildHouse() const = 0;
+    virtual House* BuildHouse() const = 0;
 
-    virtual Fence *BuildFence() const = 0;
+    virtual Fence* BuildFence() const = 0;
 
-    virtual Barne *BuildBarne() const = 0;
+    virtual Barne* BuildBarne() const = 0;
 };
 
 
-class WoodDeveloper : public Developer {
-    virtual House *BuildHouse() const override {
+class WoodDeveloper : public Developer
+{
+    virtual House* BuildHouse() const override
+    {
         return new WoodHouse();
     }
 
-    virtual Fence *BuildFence() const override {
+    virtual Fence* BuildFence() const override
+    {
         return new WoodFence();
     }
 
-    virtual Barne *BuildBarne() const override {
+    virtual Barne* BuildBarne() const override
+    {
         return new WoodBarne();
     }
 };
 
-class BrickDeveloper : public Developer {
-    virtual House *BuildHouse() const override {
+class BrickDeveloper : public Developer
+{
+    virtual House* BuildHouse() const override
+    {
         return new BrickHouse();
     }
 
-    virtual Fence *BuildFence() const override {
+    virtual Fence* BuildFence() const override
+    {
         return new BrickFence();
     }
 
-    virtual Barne *BuildBarne() const override {
+    virtual Barne* BuildBarne() const override
+    {
         return new BrickBarne();
     }
 };
 
-class ConcreteDeveloper : public Developer {
-    virtual House *BuildHouse() const override {
+class ConcreteDeveloper : public Developer
+{
+    virtual House* BuildHouse() const override
+    {
         return new ConcreteHouse();
     }
 
-    virtual Fence *BuildFence() const override {
+    virtual Fence* BuildFence() const override
+    {
         return new ConcreteFence();
     }
 
-    virtual Barne *BuildBarne() const override {
+    virtual Barne* BuildBarne() const override
+    {
         return new ConcreteBarne();
     }
 };
-
